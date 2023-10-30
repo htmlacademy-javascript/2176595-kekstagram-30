d886b97148c319b5c1be5de5089f173d0ddd03a5
Keks <keks@htmlacademy.ru>
2023-10-30 13:52:50 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/picture.js
+++ 5/js/picture.js
@@ -1,21 +1,25 @@
-import { createProfile } from './data.js';
+const renderPictures = (pictures) => {
+  const similarListPictures = document.querySelector('.pictures');
+  const similarPicturesTemplate = document.querySelector('#picture')
+    .content
+    .querySelector('.picture');
+  const similarListFragment = document.createDocumentFragment();
 
-const similarListPictures = document.querySelector('.pictures');
-const similarPicturesTemplate = document.querySelector('#picture').content;
+  pictures.forEach((pictureData) => {
+    const pictureElement = similarPicturesTemplate.cloneNode(true);
+    const pictureImage = pictureElement.querySelector('.picture__img');
+    //Адрес изображения url подставьте как атрибут src изображения.
+    pictureImage.src = pictureData.url;
+    //Описание изображения description подставьте в атрибут alt изображения.
+    pictureImage.alt = pictureData.description;
+    //Количество лайков likes выведите в блок .picture__likes.
+    pictureElement.querySelector('.picture__likes').textContent = pictureData.likes;
+    //Количество комментариев comments выведите в блок .picture__comments.
+    pictureElement.querySelector('.picture__comments').textContent = pictureData.comments.length;
+    similarListFragment.appendChild(pictureElement);
+  });
 
-const similarPictures = createProfile ();
+  similarListPictures.appendChild(similarListFragment);
+};
 
-similarPictures.forEach((pictureData) => {
-  const pictureElement = similarPicturesTemplate.cloneNode(true);
-  //Адрес изображения url подставьте как атрибут src изображения.
-  pictureElement.querySelector('.picture__img').src = pictureData.url;
-  //Описание изображения description подставьте в атрибут alt изображения.
-  pictureElement.querySelector('.picture__img').alt = pictureData.description;
-  //Количество лайков likes выведите в блок .picture__likes.
-  pictureElement.querySelector('.picture__likes').textContent = pictureData.likes;
-  //Количество комментариев comments выведите в блок .picture__comments.
-  pictureElement.querySelector('.picture__comments').textContent = pictureData.comments.length;
-  similarListPictures.appendChild(pictureElement);
-});
-
-export { similarPictures };
+export { renderPictures};
