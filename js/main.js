7570397487983cd2253a739c57234aea6c63fc73
Darina Smorkalova <darina.smorkalova@gmail.com>
2023-11-02 12:25:39 +0300

    Открытие большой картинки

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import { createProfile } from './data.js';
 import { renderPictures } from './picture.js';
+import { renderBigPicture } from './render-big-picture.js';
 
 renderPictures(createProfile());
-
-
